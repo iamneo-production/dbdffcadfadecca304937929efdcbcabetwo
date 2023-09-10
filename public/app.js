c348c027cbfee5e6add03b564362274344e640af
Iamneo <techadmin@iamneo.ai>
2023-09-10 11:36:25 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
+/*let cells = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let result = document.querySelector('.result');
 let btns = document.querySelectorAll('.btn');
@@ -61,4 +61,71 @@ btns.forEach((btn, i) => {
     btn.addEventListener('click', () => ticTacToe(btn, i));
 });
 
-document.querySelector('#reset').addEventListener('click', resetGame);
+document.querySelector('#reset').addEventListener('click', resetGame);*/
+
+const board = document.getElementById("board");
+const cells = document.querySelectorAll("[data-cell]");
+const restartButton = document.getElementById("restart-button");
+const message = document.getElementById("message");
+
+let currentPlayer = "X";
+let gameBoard = ["", "", "", "", "", "", "", "", ""];
+let gameActive = true;
+
+function checkWinner() {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            gameActive = false;
+            message.innerText = `${currentPlayer} wins!`;
+            cells[a].style.backgroundColor = cells[b].style.backgroundColor = cells[c].style.backgroundColor = "green";
+        }
+    }
+
+    if (!gameBoard.includes("") && gameActive) {
+        gameActive = false;
+        message.innerText = "It's a draw!";
+    }
+}
+
+function handleCellClick(event) {
+    const cell = event.target;
+    const cellIndex = cell.getAttribute("data-cell");
+
+    if (gameBoard[cellIndex] === "" && gameActive) {
+        gameBoard[cellIndex] = currentPlayer;
+        cell.innerText = currentPlayer;
+        cell.style.backgroundColor = "#fff";
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        checkWinner();
+    }
+}
+
+function restartGame() {
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    gameActive = true;
+    currentPlayer = "X";
+    cells.forEach((cell) => {
+        cell.innerText = "";
+        cell.style.backgroundColor = "#eee";
+    });
+    message.innerText = "";
+}
+
+cells.forEach((cell) => {
+    cell.addEventListener("click", handleCellClick);
+});
+
+restartButton.addEventListener("click", restartGame);
+
